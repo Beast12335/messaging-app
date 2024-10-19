f9858c379804c5848dcb92bd021f28157ba967a7
Beast12335 <97028489+Beast12335@users.noreply.github.com>
2024-10-19 15:43:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ def get_db_connection():
     connection = pymysql.connect(
         host=os.getenv('DB_HOST'),
         user=os.getenv('DB_USER'),
-        password=os.getenv('DB_PASSWORD'),
+        password= '',
         db=os.getenv('DB_NAME'),
         charset='utf8mb4',
         cursorclass=pymysql.cursors.DictCursor
